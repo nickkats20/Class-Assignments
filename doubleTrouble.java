d0391902b9d7eef79971bb8517758704242706e8
Kats <20KatsNi@fps.fmtdom.local>
2019-11-18 10:13:28 -0500

    first

---
+++ doubleTrouble.java
@@ -0,0 +1,15 @@
+public class doubleTrouble(){
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+    public static void  testMethod(){
+        double sum = 0.0;
+        for( int i = 0; i < 20; i++)
+       {
+       System.out.println(sum);
+       sum = sum + 0.4;
+       }
+    }
+}
