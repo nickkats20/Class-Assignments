dd7619288d9dd82194ede647ec58fddc4f25f995
Kats <20KatsNi@fps.fmtdom.local>
2020-01-29 10:31:48 -0500

    1/29

--- Bank.java
+++ Bank.java
@@ -1,50 +1,43 @@
 public class Bank {
     // fields
     private BankAccount[] accounts;
-   
 
     // constructors
 
     public Bank() {
         accounts = new BankAccount[4];
-        accounts[0] =  new BankAccount("tim barron", 300.0);
+        accounts[0] = new BankAccount("tim barron", 300.0);
         accounts[1] = new BankAccount("tim barron", 400.0);
-        accounts[2] =  new BankAccount("tim barron", 500.0);
-        accounts[3] =  new BankAccount("tim barron", 500.0);
+        accounts[2] = new BankAccount("tim barron", 500.0);
+        accounts[3] = new BankAccount("tim barron", 500.0);
     }
 
     public Bank(BankAccount[] premadeAccounts) {
-        for(int i = 0; i < accounts.length; i++)
+        this.accounts = premadeAccounts;
     }
 
-    public Bank(double startingAmountForAllAccounts) {
-        account1 = new BankAccount(startingAmountForAllAccounts);
-        account2 = new BankAccount(startingAmountForAllAccounts);
-        account3 = new BankAccount(startingAmountForAllAccounts);
+    public Bank(BankAccount[] premadeAccounts, double startingAmountForAllAccounts) {
+        this.accounts = premadeAccounts;
+        for (BankAccount b : this.accounts) b.deposit(startingAmountForAllAccounts);
     }
 
     // methods
 
     public double calcTotalFunds() {
-        return account1.getBalance() + account2.getBalance() + account3.getBalance();
+        double totalFunds = 0;
+        for (BankAccount b : this.accounts) totalFunds += b.getBalance();
+        return totalFunds;
     }
 
     public void chargeFees(double fee) {
-        account1.withdraw(fee);
-        account2.withdraw(fee);
-        account3.withdraw(fee);
+        for(BankAccount b : this.accounts) b.withdraw(fee);
     }
 
-    public void addInterest() {
-        account1.deposit(account1.getBalance() * 0.05);
-        account2.deposit(account2.getBalance() * 0.05);
-        account3.deposit(account3.getBalance() * 0.05);
+    public void addInterest(double interestRate) {
+        for(BankAccount b : this.accounts) b.deposit(b.getBalance() * interestRate);
     }
 
     public void printAllReports() {
-        account1.printInfo();
-        account2.printInfo();
-        account3.printInfo();
+       for(BankAccount b : this.accounts) b.printInfo();
     }
-
 }
